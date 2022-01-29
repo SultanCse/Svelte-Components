50e761df3b7253e6cd74a26fbbfa6bec38b31394
Sultan Ahmed <sultancse12@gmail.com>
2022-01-30 00:53:41 +0600

    Wordle frame done

--- src/components/TestCode.svelte
+++ src/components/TestCode.svelte
@@ -1,8 +1,201 @@
-<script>
-    let key='';
-    document.addEventListener('keydown',(event)=>{
-       key=event.key;
-    });
-</script>
+<div class="container w-50 mt-2">
+  <div class="row gx-1 mb-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 justify-content-center">
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-2">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+</div>
+<div class="container w-75 mt-5">
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+  <div class="row g-1 mb-1 justify-content-center">
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1 ">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+    <div class="col-1">
+      <div class="p-1 border bg-light d-flex justify-content-center">a</div>
+    </div>
+  </div>
+</div>
 
-<kbd style="padding: 1rem; font-size: 3rem">{key}</kbd>
+<style>
+  .col-2 {
+  }
+</style>
