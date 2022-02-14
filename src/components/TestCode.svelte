2c46607f2151e8df67ec38480aa2719f2419ea86
Sultan Ahmed <sultancse12@gmail.com>
2022-02-14 11:38:15 +0600

    add todo done

---
+++ src/components/TestCode.svelte
@@ -0,0 +1,32 @@
+<script>
+    let task=''
+    let todo = { task: '', completed: false, date: new Date() }
+    let todos = [{ task: 'task1', completed: false, date: new Date() }, 
+        { task: 'task2', completed: false, date: new Date() },];
+
+    const addTodo = () => {
+        if(task){
+            todo.task=task;
+            todos=[...todos,todo];
+            task='';
+        }        
+    }
+
+    const deleteTodo = (index) => {};
+    const updateTodo = (index) => {};
+    const taskCompleted = (index) => {};
+</script>
+
+<div class="d-flex justify-content-center m-2">
+  <input type="text" placeholder="add task" bind:value={task} />
+  <div class="btn btn-outline-secondary" on:click={() => addTodo()}>add</div>
+</div>
+{#each todos as todo}
+  <div class="m-2 p-2 shadow-sm">
+    <input type="checkbox" />
+    <span>{todo.task}</span>
+    <i class="fas fa-check" />
+  </div>
+{/each}
+
+<!-- {#each todos as todo} -->
---
+++ src/components/ToDoApp.svelte
@@ -0,0 +1,17 @@
+<script>
+    let todo = { title: '', completed: false, date: new Date() }
+    let todos = [];
+</script>
+
+<div class="input-group mb-3">
+  <input
+    type="text"
+    class="form-control"
+    placeholder="Recipient's username"
+    aria-label="Recipient's username"
+    aria-describedby="button-addon2"
+  />
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2"
+    >Button</button
+  >
+</div>
