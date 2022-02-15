35776a8a8ef185ba5b8b1c28496ce1a95a059832
Sultan Ahmed <sultancse12@gmail.com>
2022-02-15 20:07:55 +0600

    todo delete, completed added

--- src/components/TestCode.svelte
+++ src/components/TestCode.svelte
@@ -1,23 +1,35 @@
 <script>
     let task='';
     let completed = false;
-    let todo = { task, completed, date: new Date() }
     let todos = [{ task: 'task1', completed: false, date: new Date() }, 
         { task: 'task2', completed: false, date: new Date() },];
 
     const addTodo = () => {
         if(task){
+          let todo = { task, completed, date: new Date() }
             todo.task=task;
-            todos=[...todos,todo];
-            task='';
+            todos=[todo,...todos];
         }        
+        task='';
     }
 
-    const deleteTodo = (index) => {};
+    const deleteTodo = (index) => {
+      todos = todos.filter(item=>item!=todos[index]);
+      console.log(todos);
+    };
     const updateTodo = (index) => {};
-    const taskCompleted = (index) => {};
+    $:  completedTodo = (index) => {
+      todos[index].completed=!todos[index].completed;
+    };
+
+    const keyboardHandeler = (e)=>{
+      if(e.keyCode==13){
+        addTodo();
+      }
+    }
 </script>
 
+<svelte:window on:keydown={event => keyboardHandeler(event)} />
 <div class="d-flex justify-content-center m-2">
   <input
     type="text"
@@ -31,12 +43,22 @@
 
 <div class="d-flex justify-content-center">
   <div class="m-2 w50">
-    {#each todos as todo}
+    {#each todos as todo, index}
       <div class="row  shadow-sm justify-content-center my-1">
-        <div class="col-1"><input type="checkbox" /></div>
-        <div class="col"><p>{todo.task}</p></div>
-        <div class="col-1"><i class="fas fa-trash font-red" /></div>
+        <div class="col-1">
+          <input
+            type="checkbox"
+            bind:checked={todo.completed}
+            on:click={() => completedTodo(index)}
+          />
+        </div>
+        <div class="col" class:completed={todo.completed}>
+          <p>{todo.task}</p>
+        </div>
+        <div class="col-1 c-p" on:click={() => deleteTodo(index)}>❌</div>
       </div>
+    {:else}
+      <p class="text-center text-danger">No task added yet</p>
     {/each}
   </div>
 </div>
@@ -54,5 +76,11 @@
   }
   input:focus {
     outline: none;
+  }
+  .c-p {
+    cursor: pointer;
+  }
+  .completed {
+    text-decoration: line-through;
   }
 </style>
--- src/components/ToDoApp.svelte
+++ src/components/ToDoApp.svelte
@@ -1,17 +1,86 @@
 <script>
-    let todo = { title: '', completed: false, date: new Date() }
-    let todos = [];
+    let task='';
+    let completed = false;
+    let todos = [{ task: 'task1', completed: false, date: new Date() }, 
+        { task: 'task2', completed: false, date: new Date() },];
+
+    const addTodo = () => {
+        if(task){
+          let todo = { task, completed, date: new Date() }
+            todo.task=task;
+            todos=[todo,...todos];
+        }        
+        task='';
+    }
+
+    const deleteTodo = (index) => {
+      todos = todos.filter(item=>item!=todos[index]);
+      console.log(todos);
+    };
+    const updateTodo = (index) => {};
+    $:  completedTodo = (index) => {
+      todos[index].completed=!todos[index].completed;
+    };
+
+    const keyboardHandeler = (e)=>{
+      if(e.keyCode==13){
+        addTodo();
+      }
+    }
 </script>
 
-<div class="input-group mb-3">
+<svelte:window on:keydown={event => keyboardHandeler(event)} />
+<div class="d-flex justify-content-center m-2">
   <input
     type="text"
-    class="form-control"
-    placeholder="Recipient's username"
-    aria-label="Recipient's username"
-    aria-describedby="button-addon2"
+    placeholder="add task"
+    autofocus="autofocus"
+    bind:value={task}
+    class="w50"
   />
-  <button class="btn btn-outline-secondary" type="button" id="button-addon2"
-    >Button</button
-  >
+  <div class="btn btn-outline-secondary" on:click={() => addTodo()}>add</div>
 </div>
+
+<div class="d-flex justify-content-center">
+  <div class="m-2 w50">
+    {#each todos as todo, index}
+      <div class="row  shadow-sm justify-content-center my-1">
+        <div class="col-1">
+          <input
+            type="checkbox"
+            bind:checked={todo.completed}
+            on:click={() => completedTodo(index)}
+          />
+        </div>
+        <div class="col" class:completed={todo.completed}>
+          <p>{todo.task}</p>
+        </div>
+        <div class="col-1 c-p" on:click={() => deleteTodo(index)}>❌</div>
+      </div>
+    {:else}
+      <p class="text-center text-danger">No task added yet</p>
+    {/each}
+  </div>
+</div>
+
+<!-- {#each todos as todo} -->
+<style>
+  .w75 {
+    width: 75%;
+  }
+  .w50 {
+    width: 50%;
+  }
+  .w25 {
+    width: 25%;
+  }
+  input:focus {
+    outline: none;
+  }
+  .c-p {
+    cursor: pointer;
+  }
+  .completed {
+    text-decoration: line-through;
+  }
+</style>
